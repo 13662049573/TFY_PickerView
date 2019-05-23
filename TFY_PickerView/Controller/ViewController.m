d1936b56cbe047bd83d52f3d213d0dde38675b26
tianfengyou <420144542@qq.com>
2019-05-23 10:20:05 +0800

    youhua

--- TFY_PickerView/Controller/ViewController.m
+++ TFY_PickerView/Controller/ViewController.m
@@ -59,24 +59,50 @@
         }];
     }
     else if (btn.tag==3){
-        
+        [TFY_DatePickerView showDatePickerWithTitle:@"选择" dateType:TFY_DatePickerModeYMDHM defaultSelValue:@"" resultBlock:^(NSString * _Nonnull selectValue) {
+            NSLog(@"时间选择器1---%@",selectValue);
+        }];
     }
     else if (btn.tag==4){
-        
+        NSDate *minDate = [NSDate tfy_setHour:8 minute:10];
+        NSDate *maxDate = [NSDate tfy_setHour:20 minute:35];
+        [TFY_DatePickerView showDatePickerWithTitle:@"选择" dateType:TFY_DatePickerModeDateAndTime defaultSelValue:@"" minDate:minDate maxDate:maxDate isAutoSelect:YES themeColor:[UIColor blackColor] resultBlock:^(NSString * _Nonnull selectValue) {
+            NSLog(@"时间选择器2---%@",selectValue);
+        }];
     }
     else if (btn.tag==5){
-        
+        NSDate *minDate = [NSDate tfy_setHour:1 minute:10];
+        NSDate *maxDate = [NSDate tfy_setHour:33 minute:35];
+        [TFY_DatePickerView showDatePickerWithTitle:@"选择" dateType:TFY_DatePickerModeDate defaultSelValue:@"" minDate:minDate maxDate:maxDate isAutoSelect:YES themeColor:[UIColor blueColor] resultBlock:^(NSString * _Nonnull selectValue) {
+            NSLog(@"时间选择器3---%@",selectValue);
+        } cancelBlock:^{
+            
+        }];
     }
     else if (btn.tag==6){
-        
+        [TFY_AddressPickerView showAddressPickerWithDefaultSelected:@[@"浙江省", @"杭州市", @"西湖区"] resultBlock:^(TFY_AddressModel * _Nonnull province, CityModel * _Nonnull city, AreaModel * _Nonnull area) {
+            
+            NSLog(@"地址选择器1---%@ %@ %@",province.code,city.code,area.code);
+            
+        }];
     }
     else if (btn.tag==7){
-        
+        [TFY_AddressPickerView showAddressPickerWithDefaultSelected:@[@"浙江省", @"杭州市", @"西湖区"] isAutoSelect:NO themeColor:[UIColor brownColor] resultBlock:^(TFY_AddressModel * _Nonnull province, CityModel * _Nonnull city, AreaModel * _Nonnull area) {
+            NSLog(@"地址选择器2--%@ %@ %@",province.code,city.code,area.code);
+        }];
     }
     else if (btn.tag==8){
-        
+        [TFY_AddressPickerView showAddressPickerWithShowType:TFY_AddressPickerModeCity defaultSelected:@[@"杭州市", @"西湖区"] isAutoSelect:NO themeColor:[UIColor redColor] resultBlock:^(TFY_AddressModel * _Nonnull province, CityModel * _Nonnull city, AreaModel * _Nonnull area) {
+            NSLog(@"地址选择器3--%@ %@ %@",province.code,city.code,area.code);
+        } cancelBlock:^{
+            
+        }];
     }else if (btn.tag==9){
-        
+        [TFY_AddressPickerView showAddressPickerWithShowType:TFY_AddressPickerModeCity dataSource:nil defaultSelected:@[@"浙江省", @"杭州市", @"西湖区"] isAutoSelect:NO themeColor:[UIColor blueColor] resultBlock:^(TFY_AddressModel * _Nonnull province, CityModel * _Nonnull city, AreaModel * _Nonnull area) {
+            NSLog(@"地址选择器4--%@ %@ %@",province.code,city.code,area.code);
+        } cancelBlock:^{
+            
+        }];
     }
 }
 @end
